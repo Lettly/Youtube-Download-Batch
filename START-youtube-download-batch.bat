2faf7a75143c67806624585a04e787660487b34f
Lettly <giuseppini.marco@live.it>
2017-01-09 19:46:40 +0100

    V 1.3

--- START-youtube-download-batch.bat
+++ START-youtube-download-batch.bat
@@ -6,7 +6,7 @@ echo Made with love by Lettly
 echo.
 cd Setting
 for /f "tokens=* delims=" %%x in (DirToSave.txt) do set dir=%%x
-if %dir%=="" goto errset
+if %dir%== goto errset
 for /f "tokens=* delims=" %%x in (DownloadCopy.txt) do set dow=%%x
 if %dow%==Y goto start2
 if %dow%==y goto start2
@@ -18,8 +18,8 @@ goto start1
 cd ..
 cd Lib
 set /p id="Enter video url:"
-youtube-dl --extract-audio --audio-format "mp3" --audio-quality 0 --youtube-skip-dash-manifest -o Download\%%(title)s-%%(id)s.%%(ext)s %id%
-youtube-dl --extract-audio --audio-format "mp3" --audio-quality 0 --youtube-skip-dash-manifest -o %dir%\%%(title)s-%%(id)s.%%(ext)s %id%
+youtube-dl --extract-audio --audio-format "mp3" --audio-quality 0 --youtube-skip-dash-manifest -o Download\%%(title)s.%%(ext)s %id%
+youtube-dl --extract-audio --audio-format "mp3" --audio-quality 0 --youtube-skip-dash-manifest -o %dir%\%%(title)s.%%(ext)s %id%
 pause
 cd ..
 goto start
@@ -27,7 +27,7 @@ goto start
 :start1
 cd Lib
 set /p id="Enter video url:"
-youtube-dl --extract-audio --audio-format "mp3" --audio-quality 0 --youtube-skip-dash-manifest -o %dir%\%%(title)s-%%(id)s.%%(ext)s %id%
+youtube-dl --extract-audio --audio-format "mp3" --audio-quality 0 --youtube-skip-dash-manifest -o %dir%\%%(title)s.%%(ext)s %id%
 pause
 cd ..
 goto start
