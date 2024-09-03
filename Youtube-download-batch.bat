9fdd8f81be2d596b3266a8a7ef8c6ff8d61cea5c
LyudmilC-dacc-939 <whats_with_lusk0@abv.bg>
2024-09-03 04:31:31 +0300

    Updated Youtube-download-batch.bat
    Removed some unnecessary logic

--- Youtube-download-batch.bat
+++ Youtube-download-batch.bat
@@ -5,56 +5,69 @@ cls
 echo Made with love by Lettly
 echo.
 cd Setting
+
+rem Get the directory to save files
 for /f "tokens=* delims=" %%x in (DirToSave.txt) do set dir=%%x
-if %dir%== goto errset
+if "%dir%"=="" goto errset
+
+rem Handle download copy settings
 for /f "tokens=* delims=" %%x in (DownloadCopy.txt) do set dow=%%x
-if %dow%==Y goto start2
-if %dow%==y goto start2
-if %dow%=="" goto errset
-for /f "tokens=* delims=" %%x in (Mp3_OR_Mp4.txt) do set mp=%%x
-if %mp%==mp4 goto mp4
-if %mp%==Mp4 goto mp4
-if %mp%==mP4 goto mp4
-if %mp%==MP4 goto mp4
-if %mp%==video goto mp4
-if %mp%==Video goto mp4
-if %mp%==VIDEO goto mp4
+if /i "%dow%"=="Y" goto start2
+if /i "%dow%"=="N" goto start1
+goto errset
 
-goto start1
-
-:mp4
-cd ..
-cd Lib
-set /p id="Enter video or playlist URL: "
-echo.
-yt-dlp -F %id%
-echo.
-set /p format="Enter the format code you want to download (e.g., 22, 140): "
-yt-dlp -f %format% -o %dir%\%%(title)s.%%(ext)s %id%
-echo.
-pause
-cd ..
-goto start
-
+rem Download video or playlist
 :start2
 cd ..
 cd Lib
 set /p id="Enter video or playlist URL: "
-yt-dlp --extract-audio --audio-format "mp3" --audio-quality 0 --youtube-skip-dash-manifest -o Download\%%(title)s.%%(ext)s %id%
-yt-dlp --extract-audio --audio-format "mp3" --audio-quality 0 --youtube-skip-dash-manifest -o %dir%\%%(title)s.%%(ext)s %id%
+echo.
+yt-dlp -F %id% > formats.txt
+
+rem Display available formats
+echo Available formats:
+for /f "tokens=1,2 delims= " %%A in (formats.txt) do (
+    if "%%A" neq "format" echo %%A - %%B
+)
+echo.
+
+rem Prompt user to select a format
+set /p format="Enter the format code you want to download (e.g., 22, 140): "
+echo.
+
+rem Download the selected format
+yt-dlp -f %format% -o Download\%%(title)s.%%(ext)s %id%
+yt-dlp -f %format% -o %dir%\%%(title)s.%%(ext)s %id%
 pause
 cd ..
 goto start
 
+rem Download audio file
 :start1
 cd ..
 cd Lib
 set /p id="Enter video or playlist URL: "
-yt-dlp --extract-audio --audio-format "mp3" --audio-quality 0 --youtube-skip-dash-manifest -o %dir%\%%(title)s.%%(ext)s %id%
+echo.
+yt-dlp -F %id% > formats.txt
+
+rem Display available formats
+echo Available formats:
+for /f "tokens=1,2 delims= " %%A in (formats.txt) do (
+    if "%%A" neq "format" echo %%A - %%B
+)
+echo.
+
+rem Prompt user to select a format
+set /p format="Enter the format code you want to download (e.g., 22, 140): "
+echo.
+
+rem Download the selected format
+yt-dlp -f %format% -o %dir%\%%(title)s.%%(ext)s %id%
 pause
 cd ..
 goto start
 
+rem Handle error for empty settings
 :errset
 echo This is an empty file in the folder setting, please fill out correctly all the settings
 echo.
